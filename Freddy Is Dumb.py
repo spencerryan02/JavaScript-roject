ded12d0001cdc74d95bf81dea132583f0dbddd55
spencerryan02 <spencerryanpratt@gmail.com>
2017-05-04 11:06:55 -0400

    HomePage

---
+++ Freddy Is Dumb.py
@@ -0,0 +1,223 @@
+#Freddy’s code to write in Big Block letters.
+global bigNameLine1
+global bigNameLine2
+global bigNameLine3
+global bigNameLine4
+global bigNameLine5
+
+counter = 0
+global smallName
+smallName = 0
+
+def printBlock():
+    bigNameLine1 =[]
+    bigNameLine2 =[]
+    bigNameLine3 =[]
+    bigNameLine4 =[]
+    bigNameLine5 =[]
+    for i in smallName:
+
+        if i.lower() == 'a':
+            bigNameLine1.append("AAA  ")
+            bigNameLine2.append("A A  ")
+            bigNameLine3.append("AAA  ")
+            bigNameLine4.append("A A  ")
+            bigNameLine5.append("A A  ")      
+        if i.lower() == 'b':
+            bigNameLine1.append("BBB  ")
+            bigNameLine2.append("B B  ")
+            bigNameLine3.append("BB   ")
+            bigNameLine4.append("B B  ")
+            bigNameLine5.append("BBB  ")
+        if i.lower() == 'c':
+            bigNameLine1.append("CCC  ")
+            bigNameLine2.append("C    ")
+            bigNameLine3.append("C    ")
+            bigNameLine4.append("C    ")
+            bigNameLine5.append("CCC  ")
+        if i.lower() == 'd':
+            bigNameLine1.append("DD   ")
+            bigNameLine2.append("D D  ")
+            bigNameLine3.append("D D  ")
+            bigNameLine4.append("D D  ")
+            bigNameLine5.append("DD   ")
+        if i.lower() == 'e':
+            bigNameLine1.append("EEE  ")
+            bigNameLine2.append("E    ")
+            bigNameLine3.append("EE   ")
+            bigNameLine4.append("E    ")
+            bigNameLine5.append("EEE  ")
+        if i.lower() == 'f':
+            bigNameLine1.append("FFF  ")
+            bigNameLine2.append("F    ")
+            bigNameLine3.append("FF   ")
+            bigNameLine4.append("F    ")
+            bigNameLine5.append("F    ")
+        if i.lower() == 'g':
+            bigNameLine1.append("GGG  ")
+            bigNameLine2.append("G G  ")
+            bigNameLine3.append("G    ")
+            bigNameLine4.append("G GG ")
+            bigNameLine5.append("GGG  ")
+        if i.lower() == 'h':
+            bigNameLine1.append("H H  ")
+            bigNameLine2.append("H H  ")
+            bigNameLine3.append("HHH  ")
+            bigNameLine4.append("H H  ")
+            bigNameLine5.append("H H  ")
+        if i.lower() == 'i':
+            bigNameLine1.append("III  ")
+            bigNameLine2.append(" I   ")
+            bigNameLine3.append(" I   ")
+            bigNameLine4.append(" I   ")
+            bigNameLine5.append("III  ")
+        if i.lower() == 'j':
+            bigNameLine1.append("JJJJ ")
+            bigNameLine2.append("  J  ")
+            bigNameLine3.append("  J  ")
+            bigNameLine4.append("J J  ")
+            bigNameLine5.append(" J   ")
+        if i.lower() == 'k':
+            bigNameLine1.append("K K  ")
+            bigNameLine2.append("KK   ")
+            bigNameLine3.append("KK   ")
+            bigNameLine4.append("K K  ")
+            bigNameLine5.append("K K  ")
+        if i.lower() == 'l':
+            bigNameLine1.append("L    ")
+            bigNameLine2.append("L    ")
+            bigNameLine3.append("L    ")
+            bigNameLine4.append("L    ")
+            bigNameLine5.append("LLL  ")
+        if i.lower() == 'm':
+            bigNameLine1.append("M M  ")
+            bigNameLine2.append("MMM  ")
+            bigNameLine3.append("M M  ")
+            bigNameLine4.append("M M  ")
+            bigNameLine5.append("M M  ")
+        if i.lower() == 'n':
+            bigNameLine1.append("N  N ")
+            bigNameLine2.append("NN N ")
+            bigNameLine3.append("N NN ")
+            bigNameLine4.append("N  N ")
+            bigNameLine5.append("N  N ")
+        if i.lower() == 'o':
+            bigNameLine1.append(" O   ")
+            bigNameLine2.append("O O  ")
+            bigNameLine3.append("O O  ")
+            bigNameLine4.append("O O  ")
+            bigNameLine5.append(" O   ")
+        if i.lower() == 'p':
+            bigNameLine1.append("PP   ")
+            bigNameLine2.append("P P  ")
+            bigNameLine3.append("PP   ")
+            bigNameLine4.append("P    ")
+            bigNameLine5.append("P    ")
+        if i.lower() == 'q':
+            bigNameLine1.append(" Q   ")
+            bigNameLine2.append("Q Q  ")
+            bigNameLine3.append("Q Q  ")
+            bigNameLine4.append("Q Q  ")
+            bigNameLine5.append(" QQQ ")
+        if i.lower() == 'r':
+            bigNameLine1.append("RR   ")
+            bigNameLine2.append("R R  ")
+            bigNameLine3.append("RR   ")
+            bigNameLine4.append("R R  ")
+            bigNameLine5.append("R R  ")
+        if i.lower() == 's':
+            bigNameLine1.append("SSS  ")
+            bigNameLine2.append("S    ")
+            bigNameLine3.append("SSS  ")
+            bigNameLine4.append("  S  ")
+            bigNameLine5.append("SSS  ")
+        if i.lower() == 't':
+            bigNameLine1.append("TTT  ")
+            bigNameLine2.append(" T   ")
+            bigNameLine3.append(" T   ")
+            bigNameLine4.append(" T   ")
+            bigNameLine5.append(" T   ")
+        if i.lower() == 'u':
+            bigNameLine1.append("U U  ")
+            bigNameLine2.append("U U  ")
+            bigNameLine3.append("U U  ")
+            bigNameLine4.append("U U  ")
+            bigNameLine5.append(" U   ")
+        if i.lower() == 'v':
+            bigNameLine1.append("V V  ")
+            bigNameLine2.append("V V  ")
+            bigNameLine3.append("V V  ")
+            bigNameLine4.append(" V   ")
+            bigNameLine5.append(" V   ")
+        if i.lower() == 'w':
+            bigNameLine1.append("W W  ")
+            bigNameLine2.append("W W  ")
+            bigNameLine3.append("W W  ")
+            bigNameLine4.append("WWW  ")
+            bigNameLine5.append("W W  ")
+        if i.lower() == 'x':
+            bigNameLine1.append("X X  ")
+            bigNameLine2.append("X X  ")
+            bigNameLine3.append(" X   ")
+            bigNameLine4.append("X X  ")
+            bigNameLine5.append("X X  ")
+        if i.lower() == 'y':
+            bigNameLine1.append("Y Y  ")
+            bigNameLine2.append("Y Y  ")
+            bigNameLine3.append(" Y   ")
+            bigNameLine4.append(" Y   ")
+            bigNameLine5.append(" Y   ")
+        if i.lower() == 'z':
+            bigNameLine1.append("ZZZ  ")
+            bigNameLine2.append("  Z  ")
+            bigNameLine3.append(" Z   ")
+            bigNameLine4.append("Z    ")
+            bigNameLine5.append("ZZZ  ")
+        if i.lower() == ' ':
+            bigNameLine1.append("     ")
+            bigNameLine2.append("     ")
+            bigNameLine3.append("     ")
+            bigNameLine4.append("     ")
+            bigNameLine5.append("     ")
+        if i.lower() == '.':
+            bigNameLine1.append("    ")
+            bigNameLine2.append("    ")
+            bigNameLine3.append("    ")
+            bigNameLine4.append("    ")
+            bigNameLine5.append(" X  ")
+        if i.lower() == '?':
+            bigNameLine1.append(" ?   ")
+            bigNameLine2.append("? ?  ")
+            bigNameLine3.append("  ?  ")
+            bigNameLine4.append("     ")
+            bigNameLine5.append(" ?   ")
+    
+    bigNameLine1=''.join(bigNameLine1)
+    bigNameLine2=''.join(bigNameLine2)
+    bigNameLine3=''.join(bigNameLine3)
+    bigNameLine4=''.join(bigNameLine4)
+    bigNameLine5=''.join(bigNameLine5)
+    return bigNameLine1, bigNameLine2, bigNameLine3, bigNameLine4, bigNameLine5
+
+def printOut(bigNameLine1, bigNameLine2, bigNameLine3, bigNameLine4, bigNameLine5):
+
+    print(bigNameLine1)
+    print(bigNameLine2)
+    print(bigNameLine3)
+    print(bigNameLine4)
+    print(bigNameLine5)
+
+
+while smallName != 'no':
+    smallName = input('Would you like anything written bigger?')
+    if smallName == 'no':
+        break
+    bigNameLine1, bigNameLine2, bigNameLine3, bigNameLine4, bigNameLine5 = printBlock()
+
+    printOut(bigNameLine1, bigNameLine2, bigNameLine3, bigNameLine4, bigNameLine5)
+    
+
+
+
+
+
